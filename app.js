eb57da69ebd8f2f767f239419cc643bcb6ff7aa9
Acalltoredemption <acalltoredemption@gmail.com>
2021-11-01 18:20:37 -0700

    error handling for our routes

--- app.js
+++ app.js
@@ -7,4 +7,11 @@ const app = express();
 
 app.use('/api/places', placesRoutes);
 
+app.use((error, req, res, next) => {
+    if (res.headerSent){
+        return next(error);
+    }
+    res.status(error.code || 500);
+});
+
 app.listen(5000);
